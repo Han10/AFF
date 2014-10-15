faa59933c29b0da188952f4ede5a733b8cb44e51
mtuck063 <meneliktucker@gmail.com>
2014-10-14 20:56:45 -0400

    Commit again...

--- AFF/ThirdViewController.swift
+++ AFF/ThirdViewController.swift
@@ -1,26 +1,26 @@
 //
-//  FirstViewController.swift
+//  ThirdViewController.swift
 //  AFF
 //
-//  Created by Yilin H on 2014-10-03.
+//  Created by Mac on 2014-10-13.
 //  Copyright (c) 2014 Yilin H. All rights reserved.
 //
 
+
 import UIKit
 
-class FirstViewController: UIViewController {
+class ThirdViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-
+    
 }
+
